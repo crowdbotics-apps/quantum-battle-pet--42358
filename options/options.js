8f10ece05c40a3f12ceec20d749ea5b3fb89150d
Crowdbotics <team@crowdbotics.com>
2023-07-22 21:53:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quantum-battle-pet--42358",
+  url: "https://quantum-battle-pet--42358.botics.co",
+  api: "https://quantum-battle-pet--42358.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
